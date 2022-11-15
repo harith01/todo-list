88ac19dcb28a8115667e253562412fc7a442c9b1
harith01 <harithyusuph@gmail.com>
2022-11-15 16:32:17 +0100

    Declare hideform function

--- src/form.js
+++ src/form.js
@@ -5,4 +5,11 @@ function displayForm(){
     addProject.classList.add('hide');
 }
 
+export function hideForm(){
+    const form = document.querySelector('#addProjectForm');
+    const addProject = document.querySelector('#addProject');
+    form.classList.add('hide');
+    addProject.classList.remove('hide');
+}
+
 export default displayForm
