f1acbe41107e8990009875ccdd15db4ef0e89808
harith01 <harithyusuph@gmail.com>
2022-11-15 10:12:07 +0100

    Insert Task Icon

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import print from './print.js'
+import print from './print.js';
+import Icon from './img/task.svg';
+import './style/style.css'
+
+
+const myImage = new Image();
+myImage.src = Icon;
+const header = document.querySelector('header');
+const title = document.querySelector('.header-title');
+header.insertBefore(myImage,title);
 
 print()
